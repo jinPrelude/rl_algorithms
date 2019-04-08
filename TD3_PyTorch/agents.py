5e636e2b7e16cb1bf1becb16c52d874083a80ec9
park <jin.Prelude@gmail.com>
2019-04-08 16:58:10 +0900

    cleaningup code

--- TD3_PyTorch/agents.py
+++ TD3_PyTorch/agents.py
@@ -1,5 +1,5 @@
 import random
-from memory import Per, Memory
+from memory import Memory
 
 import numpy as np
 import torch
@@ -156,110 +156,5 @@ class TD3(nn.Module):
         self.actor_optimizer.step()
 
         self.update_target_model()
-
-class TD3_PER(nn.Module):
-    def __init__(self, args, action_dim, max_action, state_dim, device):
-        super(TD3_PER, self).__init__()
-        self.actor = Actor(state_dim, action_dim).to(device)
-        self.target_actor = Actor(state_dim, action_dim).to(device)
-        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=args['actor_lr'])
-
-        self.critic1 = Critic(state_dim, action_dim).to(device)
-        self.target_critic1 = Critic(state_dim, action_dim).to(device)
-        self.critic2 = Critic(state_dim, action_dim).to(device)
-        self.target_critic2 = Critic(state_dim, action_dim).to(device)
-        self.critic_optimizer1 = optim.Adam(self.critic1.parameters(), lr=args['critic_lr'])
-        self.critic_optimizer2 = optim.Adam(self.critic2.parameters(), lr=args['critic_lr'])
-
-        self.memory = Per(args['memory_size'])
-        self.memory_counter = 0
-
-        self.ou = OrnsteinUhlenbeckActionNoise(action_dim=action_dim)
-
-        self.args = args
-        self.action_dim = action_dim
-        self.max_action = max_action
-        self.state_dim = state_dim
-        self.device = device
-
-    def noise(self, delta=0.1):
-        return np.random.normal(0, delta, self.action_dim)
-
-    def get_action(self, state):
-        state = torch.Tensor(state).to(self.device)
-        model_action = self.actor(state).cpu().detach().numpy()
-        if self.args['noise_type'] == 'ou' :
-            action = model_action + self.ou.sample()
-        elif self.args['noise_type'] == 'gaussian' :
-            action = model_action + self.noise(self.args['noise_delta'])
-        elif self.args['noise_type'] == 'none' :
-            action = model_action
-        return action*self.max_action
-
-    def update_target_model(self):
-        target = [self.target_actor, self.target_critic1, self.target_critic2]
-        source = [self.actor,self.critic1, self.critic2]
-        for t,s in zip(target,source) :
-            for target_param, param in zip(t.parameters(), s.parameters()):
-                target_param.data.copy_(target_param.data * (1.0 - self.args['tau']) + param.data * self.args['tau'])
-
-    def save(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-        self.memory_counter += 1
-
-
-    def train(self):
-        minibatch, idxs, is_weights = self.memory.sample(self.args['batch_size'])
-        minibatch = np.array(minibatch).transpose()
-
-        states = torch.from_numpy(np.vstack(minibatch[0])).float()
-        actions = torch.from_numpy(np.vstack(minibatch[1])).float()
-        rewards = torch.from_numpy(np.vstack(minibatch[2])).float()
-        next_states = torch.from_numpy(np.vstack(minibatch[3])).float()
-        dones = torch.from_numpy(np.vstack(minibatch[4].astype(int))).float()
-
-        states = torch.Tensor(states).to(self.device)
-        actions = torch.Tensor(actions).to(self.device)
-        rewards = torch.Tensor(rewards).to(self.device)
-        next_states = torch.Tensor(next_states).to(self.device)
-        dones = torch.Tensor(dones).to(self.device)
-
-        # critic update
-        smoothing_noise = torch.normal(torch.zeros(self.args['batch_size'], self.action_dim), self.args['noise_delta'])
-        target_next_a = self.target_actor(next_states) + torch.Tensor(smoothing_noise).to(self.device)
-        target_next_a = target_next_a.clamp(-self.max_action, self.max_action)
-        target_q1 = self.target_critic1(next_states, target_next_a)
-        target_q2 = self.target_critic2(next_states, target_next_a)
-        target_q = torch.min(target_q1, target_q2)
-        target = rewards + ((1 - dones) * self.args['gamma'] * target_q).detach() # detach()를 때면 오류 원인 모
-
-        q1 = self.critic1(states, actions)
-        critic_loss1 = F.mse_loss(q1, target)
-        self.critic_optimizer1.zero_grad()
-        critic_loss1.backward()
-        self.critic_optimizer1.step()
-
-        # per update
-        errors = torch.abs(q1 - target)
-        errors = torch.squeeze(errors).cpu().detach().numpy()
-        for i in range(self.args['batch_size']):
-            idx = idxs[i]
-            self.memory.update(idx, errors[i])
-
-        # critic 2 update
-        q2 = self.critic2(states, actions)
-        critic_loss2 = F.mse_loss(q2, target)
-        self.critic_optimizer2.zero_grad()
-        critic_loss2.backward()
-        self.critic_optimizer2.step()
-
-        # actor update
-        self.actor_optimizer.zero_grad()
-        actor_loss = -self.critic1(states, self.actor(states)).mean()
-        actor_loss.backward()
-        self.actor_optimizer.step()
-
-        self.update_target_model()
-
 
 
--- TD3_PyTorch/evaluate.py
+++ TD3_PyTorch/evaluate.py
@@ -17,7 +17,8 @@ def main(args):
 
 
     td3 = TD3(args, action_dim, max_action, state_dim, device)
-    summary = tensorboardX.SummaryWriter('./log/{}_td3_{}'.format(args['env_name'], args['noise_type']))
+    trained_actor = torch.load(args['model_directory'])
+    td3.actor.load_state_dict(trained_actor)
 
     timestep = 0
     for episode in range(args['max_episode']):
@@ -27,25 +28,14 @@ def main(args):
         while True:
             action = td3.get_action(state)
             next_state, reward, done, info = env.step(action)
-            td3.save(state, action, reward, next_state, int(done))
+            env.render()
             episode_reward += reward
             state = next_state
             timestep += 1
 
-            if td3.memory_counter > args['train_start_timestep']:
-                if timestep % 2 == 0 :
-                    td3.train()
-
             if done:
                 print('episode: ', episode, '   reward : %.3f'%(episode_reward), '    timestep :', timestep)
-                summary.add_scalar('reward/episode', episode_reward, episode)
-
                 break
-
-        if episode % args['save_freq'] == 0:
-            if not os.path.exists('./SaveModel') :
-                os.mkdir('./SaveModel')
-            torch.save(td3.actor.state_dict(), './SaveModel/{}_td3_{}_{}'.format(args['env_name'], args['noise_type'], episode))
 
 
 if __name__ == '__main__':
@@ -53,22 +43,23 @@ if __name__ == '__main__':
 
     parser.add_argument('--env-name', default='LunarLanderContinuous-v2')
     parser.add_argument('--env-seed', default=0)
-    parser.add_argument('--render', default=False, type=bool)
+    parser.add_argument('--render', default=True, type=bool)
     parser.add_argument('--evaluate', default=False, type=bool)
-    parser.add_argument('--model-directory', default='./SaveModel/Pendulum-v0_210', type=str)
-    parser.add_argument('--max-episode', default=1000000)
+    parser.add_argument('--model-directory', default='./SaveModel/LunarLanderContinuous-v2_gaussian_100', type=str)
+    parser.add_argument('--max-episode', default=5000)
     parser.add_argument('--save-freq', default=100)
 
 
-    parser.add_argument('--actor-lr', default=1e-3)
-    parser.add_argument('--critic-lr', default=1e-3)
+    parser.add_argument('--actor-lr', default=0.001)
+    parser.add_argument('--critic-lr', default=0.001)
     parser.add_argument('--gamma', default=0.99)
-    parser.add_argument('--memory-size', default=1000000)
-    parser.add_argument('--noise_type', default='gaussian')
-    parser.add_argument('--noise-delta', default=0.1)
+    parser.add_argument('--noise-timestep', default=10000)
+    parser.add_argument('--memory-size', default=200000)
+    parser.add_argument('--noise_type', default='none')
+    parser.add_argument('--noise-delta', default=1.0)
     parser.add_argument('--batch-size', default=32)
-    parser.add_argument('--train-start-timestep', default=50000)
-    parser.add_argument('--tau', default=5e-3)
+    parser.add_argument('--random-action-timestep', default=3000)
+    parser.add_argument('--tau', default=1e-3)
 
 
     args = vars(parser.parse_args())
